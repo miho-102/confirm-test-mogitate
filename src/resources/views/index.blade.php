b0f2d30e4c4c888573d6b02d4034d56726558b26
kobayashi miho <s0l.bang1024@icloud.com>
2026-05-12 00:42:38 +0900

    ファイルの追加

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,7 +11,7 @@
         <h1 class="product-list__title">
             商品一覧
         </h1>
-        <form action="{{ route('products.index') }}" method="GET">
+        <form action="/products" method="GET">
             <input type="text" name="keyword" class="search-form__input" placeholder="商品名で検索">
             <button class="search-form__button">
                 検索
@@ -32,45 +32,35 @@
     </div>
     <div class="product-list__content">
         <div class="product-list__header">
-            <a href="{{ route('products.create') }}" class="add-button">
+            <a href="#" class="add-button">
                 + 商品を追加
             </a>
         </div>
-        <div class="product-card__wrap">
+            <div class="product-card__wrap">
+                @foreach ($products as $product)
+                <div class="product-card">
+                    <img
+                    src="{{ asset($product['image']) }}"
+                    alt="{{ $product['name'] }}"
+                    class="product-card__image">
 
+                    <div class="product-card__content">
+                        <p class="product-card__name">
+                            {{ $product['name'] }}
+                        </p>
+                        <p class="product-card__price">
+                            ¥{{ number_format($product['price']) }}
+                        </p>
+                    </div>
+                </div>
 
-            @php
-                $products = [
-                    [
-                        'name' => 'キウイ',
-                        'price' => 800,
-                        'image' => 'https://images.unsplash.com/photo-1585059895524-72359e06133a'
-                    ],
-                    [
-                        'name' => 'ストロベリー',
-                        'price' => 1200,
-                        'image' => 'https://images.unsplash.com/photo-1464965911861-746a04b4bca6'
-                    ],
-                    [
-                        'name' => 'オレンジ',
-                        'price' => 850,
-                        'image' => 'https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b'
-                    ],
-                    [
-                        'name' => 'スイカ',
-                        'price' => 700,
-                        'image' => 'https://images.unsplash.com/photo-1563114773-84221bd62daa'
-                    ],
-                    [
-                        'name' => 'ピーチ',
-                        'price' => 1000,
-                        'image' => 'https://images.unsplash.com/photo-1629828874514-3f4b2c4d4f5b'
-                    ],
-                    [
-                        'name' => 'シャインマスカット',
-                        'price' => 1400,
-                        'image' => 'https://images.unsplash.com/photo-1537640538966-79f369143f8f'
-                    ],
-                ];
-            @endphp
+            @endforeach
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
+
 
